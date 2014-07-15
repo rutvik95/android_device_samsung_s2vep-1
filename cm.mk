be5d61710ba762b7be62ffb76503d29dc0130be6
Rutvik Rajagopal <rutvik_95@yahoo.co.in>
2014-07-15 21:52:24 +0530

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,18 +1,18 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/axxion/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := GT-I9105P
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/axxion/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/s2vep/device_s2vep.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := s2vep
-PRODUCT_NAME := cm_s2vep
+PRODUCT_NAME := axxion_s2vep
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-I9105P
